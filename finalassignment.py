2cdb620e82fb9dec286e325bf992c52f147c72d3
TLZ905 <111936584+TLZ905@users.noreply.github.com>
2022-08-24 13:36:47 +0800

    Delete finalassignment.py

--- finalassignment.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-input = ['Iris-setosa','Iris-versicolor','Iris-virginica']
-num = np.array(input)
-reshaped = num.reshape(3,1)
-pd.DataFrame(reshaped, columns=['Species'])
