534a7ea792f22c570a7b4ff2b157af6543e38ea6
TLZ905 <111936584+TLZ905@users.noreply.github.com>
2022-08-24 13:52:53 +0800

    Update finalassignment.py

--- finalassignment.py
+++ finalassignment.py
@@ -7,3 +7,5 @@ st.write(pd.DataFrame({
     'No': ['0', '1', '2'],
     'Species': ['Iris-sentosa', 'Iris-versicolor', 'Iris-virginica']
 }))
+
+st.image("C:\Users\User\Downloads\Just be a rock.jpg", caption='Just be a rock', width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
