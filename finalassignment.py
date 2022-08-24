3a7f39e7248469c4c8d8ea905317f5baa3d19f73
TLZ905 <111936584+TLZ905@users.noreply.github.com>
2022-08-24 13:37:02 +0800

    Add files via upload

---
+++ finalassignment.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("Final Assignment")
+st.write(pd.DataFrame({
+    'No': ['0', '1', '2'],
+    'Species': ['Iris-sentosa', 'Iris-versicolor', 'Iris-virginica']
+}))
